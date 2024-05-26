f76f4149175795ba68afdac282473e59fa3ef09e
Nicolas <nicolascamara29@gmail.com>
2024-05-26 01:21:58 -0700

    Update page.tsx

--- src/app/test-embed/page.tsx
+++ src/app/test-embed/page.tsx
@@ -1,8 +1,8 @@
 export default function TestEmbed() {
   return (
     <iframe
-      src="https://firegraph-xi.vercel.app/embed?padding=32&theme=%7B%22name%22%3A%22firecrawl%22%2C%22gradient%2223A%22linear-gradient(135deg%2C%20%23fdba74%2C%20%23f97316)%22%2C%22startColor%22%3A%22%23fdba74%22%2C%22endColor%22%3A%22%23f97316%22%2C%22color%22%3A%22orange%22%7D&background=true&darkMode=false"
-      style={{ border: "none", width: "100%", height: "400px" }}
+      src="https://firegraph-xi.vercel.app/embed?padding=32&theme=firecrawl&background=true&darkMode=false"
+      style={{ border: "none", width: "800px", height: "500px" }}
       allowFullScreen
     ></iframe>
   );
