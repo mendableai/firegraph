0e28688778deecc97779ed6ef449350d4896bd93
Nicolas <nicolascamara29@gmail.com>
2024-05-27 13:28:45 -0700

    Nick:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
   const githubStars = await useGithubStars();
   return (
     <div className="relative">
-      <div className="z-10 absolute top-4 right-4 p-4">
+      <div className="hidden md:flex z-10 absolute top-4 right-4 p-4">
         <GithubButton githubStars={githubStars} />
       </div>
       <MainComponent />
